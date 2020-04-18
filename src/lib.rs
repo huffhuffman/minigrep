ad86dc691053c11b63ec3a7622c836b5ffb5696a
havveFn <46079709+havveFn@users.noreply.github.com>
2020-04-18 15:45:26 +0900

    feat: search を実装

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,61 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub struct Config {
-  pub query: String,
-  pub filename: String,
+    pub query: String,
+    pub filename: String,
 }
 
 impl Config {
-  pub fn new(args: &[String]) -> Result<Config, &'static str> {
-    if args.len() < 3 {
-      return Err("not enough arguments");
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
     }
-
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Ok(Config { query, filename })
-  }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-  let mut f = File::open(config.filename)?;
+    let mut f = File::open(config.filename)?;
 
-  let mut contents = String::new();
-  f.read_to_string(&mut contents)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
 
-  println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
-  Ok(())
+    Ok(())
 }
